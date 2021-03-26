cfeedfe521529ddfdcabfcb6b2db20426974b8b3
frankie nomad <d,hollowind@gmail.com>
2021-03-26 16:12:27 +0000

    initial states-in-react commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import {Component, useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+  super();
+  this.state = {age: 31};
+}
+handleClick =()=> {
+  console.log("Button Clicked!");
+  let newAge = this.state.age + 1;
+  this.setState({age: newAge})
 }
 
-export default App;
+render(){
+  return(
+    <div>
+      <h1>Age is {this.state.age}</h1>
+      <button onClick={this.handleClick}>Click Me!</button>
+      </div>
+     );
+   }
+ }
+
+// function App(){
+//   const [state, setState] = useState({age: 20})
+//   const handleClick =()=>{
+//     let newAge = state.age + 1;
+//     setState({age: newAge});
+//   }
+//   return(
+//     <div>
+//       <h1>Age is {state.age}</h1>
+//       <button onClick={handleClick}>Click Me!</button>
+//     </div>
+//   );
+// }
+
+export default App
